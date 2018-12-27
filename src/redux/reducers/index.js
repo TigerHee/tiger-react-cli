774f28bcec90372d20a5da86bc7e775ca9e8fbe5
hehu <316031602@qq.com>
2018-12-27 16:44:16 +0800

    redux修改

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import common from './common/common'
+import other from './other/other'
 
 export default combineReducers({
-  common
+  common,
+  other
 })
